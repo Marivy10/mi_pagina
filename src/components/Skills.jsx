988bffc12de2f7261cfcfda854819ee939eba3a0
santiagoweb212 <juanusca45@gmail.com>
2023-01-29 21:03:50 -0500

    structura skill modificado

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,30 @@
 import React from "react";
-import estilo from "../styles/skills.module.css";
+import estilos from "../styles/skills.module.css";
 import TextShpere from "./TextShpere";
 
 function Skills() {
   return (
+    <div className={estilos.containerP}>
+      <h1 className={estilos.titleSkill}>skills</h1>
+      <div className={estilos.containerP_containerSphera}>
+        <div className={estilos.containerTextoParrafo}>
+          <p className={estilos.parrafoDescription}>
+            Full Stack developer working in React.Js, Redux, Node.Js
+            amoung other in the sector, I carried out projects where
+            the Frontend with responsive pages could be observed, and
+            the backend of the same.
+          </p>
+          {/* <p className={estilos.parrafoDescription}>
+            I AM with good interpersonal relationships: Proactive
+            Organize responsible person Good with personal
+            relationships
+          </p> */}
+        </div>
+        <TextShpere />
+      </div>
+    </div>
+  );
+  /*  return (
     <div className={estilo.contenedorMayor}>
       <div className={estilo.contenedorSecundario}>
         <div className={estilo.posicion}>
@@ -33,7 +54,7 @@ function Skills() {
         </ul>
       </div>
     </div>
-  );
+  ); */
 }
 
 export default Skills;
