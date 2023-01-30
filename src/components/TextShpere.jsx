85feca76dc5b2007b4e5a1731ba6ee6344ddd4a0
santiagoweb212 <juanusca45@gmail.com>
2023-01-29 21:04:04 -0500

    estructura modificado

--- src/components/TextShpere.jsx
+++ src/components/TextShpere.jsx
@@ -1,43 +1,38 @@
 import React from "react";
-import estilos from "../styles/TextShpere.module.css"
+import estilos from "../styles/TextShpere.module.css";
 import { TagCloud } from "@frank-mayer/react-tag-cloud";
 // same as: import TagCloud from "@frank-mayer/react-tag-cloud"
 
+const TextShpere = () => {
+  const data = [
+    "JavaScript",
+    "ReactJs",
+    "Figma",
+    "Bootstrap",
+    "HTML",
+    "CSS",
+    "Redux",
+    "Node",
+    "Express.Js",
+    "mongo",
+    "posgresSQL",
+    "mySQL",
+  ];
 
-
-const TextShpere = () => (
-  <div className={estilos.contendorPrincipal}>
-    <div className={estilos.contenedorSecundario}>
-      <div className={estilos.esfera}>
-        <TagCloud className={estilos.tam}
-        /*  options={(w: Window & typeof globalThis): TagCloudOptions => ({
-            radius: Math.min(500, w.innerWidth, w.innerHeight) / 2,
-            maxSpeed: "fast",
-        })} */
-        /*   onClick={(tag: string, ev: MouseEvent) => alert(tag)} */
-        /*   onClickOptions={{ passive: true }} */
-/*         options={radius= "200"} */
-        >
-
-          {[
-          
-            "JavaScript",
-            "ReactJs",
-            "Figma",
-            "Bootstrap",
-            "HTML",
-            "CSS",
-            "Redux",
-            "Node",
-            "Express.Js",
-            "mongo",
-            "posgresSQL",
-            "mySQL",
-          ]}
-        </TagCloud>
-      </div>
+  return (
+    <div className={estilos.contenedorPrincipal}>
+      <TagCloud
+      className={estilos.spheraStyle}
+        options={(w) => ({
+          radius: Math.min(500, w.innerWidth, w.innerHeight) / 2,
+          maxSpeed: "fast",
+        })}
+        onClick={(tag, ev) => alert(tag)}
+        onClickOptions={{ passive: true }}>
+        {data}
+      </TagCloud>
     </div>
-  </div>
-);
+  );
+};
 
 export default TextShpere;
